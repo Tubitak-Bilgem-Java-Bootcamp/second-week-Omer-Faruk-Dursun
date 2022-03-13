61d718133115f660265a8168ae6220bf7a544a11
Omer-Faruk-Dursun <omer.farukdursun@outlook.com>
2022-03-13 12:28:43 +0300

    Add basic structure and base classes for the game

---
+++ adventure-game/src/main/java/Game.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Game {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void start(){
+        System.out.println("Weolcome to this adventure game !");
+        System.out.println("Please enter the name of your character:");
+
+        String playerName = scanner.nextLine();
+    }
+}
